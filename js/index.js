ccb325ae88210d26f1366010047c140507530ef6
jameshhuz <jamestorresxd@gmail.com>
2023-07-27 16:45:27 -0500

    cambio

---
+++ js/index.js
@@ -0,0 +1,26 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.menu', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '-100px'
+    });
+
+    sr.reveal('.header-content', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '-100px'
+    });
+
+
+    sr.reveal('.blog-content', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '-100px'
+    });
+
+    sr.reveal('.coffee-group', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '-100px'
+    });
